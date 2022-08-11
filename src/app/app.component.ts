fb3f20b18686fd17ea63944bc41085ef8c5f475f
Juan Bigois <jcrbigois@hotmail.com>
2022-08-11 15:53:04 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
+import { PoMenuItem } from '@po-ui/ng-components';
+
 @Component({
+ 
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-word';
+  input!: string;
+  cep!:string;
+  readonly  menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) }
+  ];
+  constructor(private http:HttpClient){
+  
+  }
+    onClick() {
+      this.http.get(`https://brasilapi.com.br/api/cnpj/v1/${this.input}`).subscribe((value:any)=>{
+        console.log(value.cnpj,value.cep,value)
+        this.cep=value.cep
+      })
+      console.log(this.input)
+    alert('Clicked in menu item')
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    PoModule,
+    RouterModule.forRoot([]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
